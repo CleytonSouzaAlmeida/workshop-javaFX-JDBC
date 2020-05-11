8c236575f0e334042009a2f3a2610430bb9bdd76
CleytonSouzaAlmeida <cleytoneaa@gmail.com>
2020-05-11 20:21:22 -0300

    Saving a new department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,18 +1,26 @@
 
 package gui;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 
+    private DepartmentService service;
+    
     private Department entity;
     
     @FXML
@@ -34,14 +42,31 @@ public class DepartmentFormController implements Initializable{
         this.entity = entity;
     }
     
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
+    }
+    
     @FXML
-    public void onBtSaveAction(){
-        System.out.println("onBtSaveAction");
+    public void onBtSaveAction(ActionEvent event){
+        if (entity == null){
+            throw new IllegalStateException("Entity was null!");
+        }
+        if (service == null){
+            throw new IllegalStateException("Service was null!");
+        }
+        try{
+            entity = getFormDate();
+            service.saveOrUpdate(entity);
+        }
+        catch(DbException e){
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+        Utils.currentStage(event).close();
     }
         
     @FXML
-    public void onBtCancelAction(){
-        System.out.println("onBtCancelAction");
+    public void onBtCancelAction(ActionEvent event){
+        Utils.currentStage(event).close();
     }
     
     @Override
@@ -60,6 +85,15 @@ public class DepartmentFormController implements Initializable{
         }
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
+    }
+
+    private Department getFormDate() {
+        Department obj = new Department();
+        
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setName(txtName.getText());
+        
+        return obj;
     }
     
 }
